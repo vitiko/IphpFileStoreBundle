34d30f05c24907e1f171862b67d72d1d93f5c85c
vitiko <vitiko@mail.ru>
2013-05-22 11:48:22 +0400

    fix bootstrap

--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,11 +1,13 @@
 <?php
 
-$autoloadFile = __DIR__.'/../../../../../autoload.php';
+$autoloadFiles = array ( __DIR__.'/../vendor/autoload.php', __DIR__.'/../../../../../autoload.php');
 
-require $autoloadFile;
-return;
-
-//require_once __DIR__.'/../vendor/autoload.php';
-if (!is_file($autoloadFile = __DIR__.'/../vendor/autoload.php')) {
-    throw new \LogicException('Could not find autoload.php in vendor/. Did you run "composer install --dev"?');
+foreach ($autoloadFiles as $autoloadFile)
+{
+    if (!is_file($autoloadFile)) continue;
+ 
+    require $autoloadFile;
+    return;
 }
+
+throw new \LogicException('Could not find autoload.php in vendor/. Did you run "composer install --dev"?');
