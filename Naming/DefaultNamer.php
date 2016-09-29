e5366c93331c246866c58409e7fca36cc6a6ffc9
vitiko <vitiko@mail.ru>
2016-09-30 00:19:43 +0300

    transliteration only via transliterator_transliterate (5.4)

--- Naming/DefaultNamer.php
+++ Naming/DefaultNamer.php
@@ -17,55 +17,10 @@ class DefaultNamer
      */
     public function translitRename(PropertyMapping $propertyMapping, $name)
     {
-
-
-
-
-        if (function_exists('transliterator_transliterate')) {
-            $name = transliterator_transliterate("Any-Latin; Latin-ASCII; [\u0100-\u7fff] remove" , $name);
-            $name = preg_replace('/[^\\pL\d.]+/u', '-', $name);
-            $name = preg_replace('/[-\s]+/', '-', $name);
-        } else {
-
-
-            $iso = array(
-                "Є" => "YE", "І" => "I", "Ѓ" => "G", "і" => "i", "№" => "N", "є" => "ye", "ѓ" => "g",
-                "А" => "A", "Б" => "B", "В" => "V", "Г" => "G", "Д" => "D",
-                "Е" => "E", "Ё" => "e", "Ж" => "z",
-                "З" => "Z", "И" => "I", "Й" => "J", "К" => "K", "Л" => "L",
-                "М" => "M", "Н" => "N", "О" => "O", "П" => "P", "Р" => "R",
-                "С" => "S", "Т" => "T", "У" => "U", "Ф" => "F", "Х" => "H",
-                "Ц" => "C", "Ч" => "C", "Ш" => "S", "Щ" => "s", "Ъ" => "",
-                "Ы" => "Y", "Ь" => "", "Э" => "E", "Ю" => "U", "Я" => "a",
-                "а" => "a", "б" => "b", "в" => "v", "г" => "g", "д" => "d",
-                "е" => "e", "ё" => "e", "ж" => "z",
-                "з" => "z", "и" => "i", "й" => "j", "к" => "k", "л" => "l",
-                "м" => "m", "н" => "n", "о" => "o", "п" => "p", "р" => "r",
-                "с" => "s", "т" => "t", "у" => "u", "ф" => "f", "х" => "h",
-                "ц" => "c", "ч" => "c", "ш" => "s", "щ" => "s", "ъ" => "",
-                "ы" => "y", "ь" => "", "э" => "e", "ю" => "u", "я" => "a", "«" => "", "»" => "", "—" => "-"
-            );
-            $name = strtr($name, $iso);
-
-
-            $name = preg_replace('/[^\\pL\d.]+/u', '-', $name);
-            $name = preg_replace('/[-\s]+/', '-', $name);
-
-            // transliterate
-            if (function_exists('iconv')) {
-                $name = iconv('utf-8', 'ASCII//TRANSLIT//IGNORE', $name);
-            }
-
-            $name = preg_replace("/[^0-9A-Za-z-_ .]/", "", $name);
-
-
-
-        }
-
-        $name = trim($name, '-');
-        $name = strtolower($name);
-
-
+        $name = transliterator_transliterate("Any-Latin; Latin-ASCII; [\u0100-\u7fff] remove", $name);
+        $name = preg_replace('/[^\\pL\d.]+/u', '-', $name);
+        $name = preg_replace('/[-\s]+/', '-', $name);
+        $name = strtolower(trim($name, '-'));
 
         return $name;
     }
